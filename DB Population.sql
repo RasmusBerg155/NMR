094e8d6b7eca2bd30c3e13ae54c2cbe779000955
Wildcatze <65553747+Wildcatze@users.noreply.github.com>
2020-05-25 12:13:03 +0200

    Add files via upload

---
+++ DB Population.sql
@@ -0,0 +1,120 @@
+INSERT INTO `nordic-motorhome`.`motorhome_details` VALUES
+(1, 2463, 2, '79$', 'x2 43'' TV Plasma, extra storage, fire place'),
+(2, 22450, 2, '79$', 'x2 43'' TV Plasma, extra storage, fire place'),
+(3, 1200, 2, '79$', 'x2 43'' TV Plasma, extra storage, fire place'),
+(4, 3500, 2, '79$', 'x2 43'' TV Plasma, extra storage, fire place'),
+(5, 4785, 3, '82$', '2 toilets, extra refrigerator space, microwave, 43'' let tv + fireplace'),
+(6, 23568, 3, '82$', '2 toilets, extra refrigerator space, microwave, 43'' let tv + fireplace'),
+(7, 2323, 3, '82$', '2 toilets, extra refrigerator space, microwave, 43'' let tv + fireplace'),
+(8, 2887, 2, '80$', 'leath`motorhome details`er interior, extra large sofa, changing room, driver and passanger pedestal table, bathtub'),
+(9, 4567, 2, '80$', 'leather interior, extra large sofa, changing room, driver and passanger pedestal table, bathtub'),
+(10, 13675, 2, '80$', 'leather interior, extra large sofa, changing room, driver and passanger pedestal table, bathtub'),
+(11, 53557, 4, '75$', 'extra comfortable driver and passenger seats, pedestal table for the driver and passenger, 2 beds, large electric stove, x2 bike racks'),
+(12, 53532, 4, '75$', 'extra comfortable driver and passenger seats, pedestal table for the driver and passenger, 2 beds, large electric stove, x2 bike racks'),
+(13, 9778, 4, '75$', 'extra comfortable driver and passenger seats, pedestal table for the driver and passenger, 2 beds, large electric stove, x2 bike racks'),
+(14, 21057, 5, '220$', '2 bathrooms, x2 32'' LED TVs + 1 pop-up 55'' LET TV, extra large refrigerator, Oxford wood and leather interior'),
+(15, 5464, 5, '220$', '2 bathrooms, x2 32'' LED TVs + 1 pop-up 55'' LET TV, extra large refrigerator, Oxford wood and leather interior'),
+(16, 4546, 2, '69$', 'wheelchair entrance, low fuel consumption, flip-up computer table'),
+(17, 54654, 2, '69$', 'wheelchair entrance, low fuel consumption, flip-up computer table'),
+(18, 9787, 2, '69$', 'wheelchair entrance, low fuel consumption, flip-up computer table'),
+(19, 4668, 2, '110$', '2 bathrooms with an extra large bathroom with bathtub, x2 4K LET TVs, extra spatious'),
+(20, 87546, 2, '110$', '2 bathrooms with an extra large bathroom with bathtub, x2 4K LET TVs, extra spatious'),
+(21, 5465, 2, '110$', '2 bathrooms with an extra large bathroom with bathtub, x2 4K LET TVs, extra spatious'),
+(22, 55656, 2, '110$', '2 bathrooms, extra kitchen space, x2 50'' 4K and LED TVs, memory foam bed'),
+(23, 8868, 2, '110$', '2 bathrooms, extra kitchen space, x2 50'' 4K and LED TVs, memory foam bed'),
+(24, 4005, 2, '110$', '2 bathrooms, extra kitchen space, x2 50'' 4K and LED TVs, memory foam bed'),
+(25, 6858, 4, '100$', 'Open hatch on the roof to mount a 2pers. sleeping tent, low fuel consumption'),
+(26, 875, 4, '100$', 'Open hatch on the roof to mount a 2pers. sleeping tent, low fuel consumption'),
+(27, 49752, 4, '100$', 'Open hatch on the roof to mount a 2pers. sleeping tent, low fuel consumption'),
+(28, 4524, 4, '100$', 'Open hatch on the roof to mount a 2pers. sleeping tent, low fuel consumption'),
+(29, 124488, 2, '145$', '4WD and off road suspension and tires, interior heating, extending roof for mounting an outdoor terrace, 2 bike racks'),
+(30, 45655, 2, '145$', '4WD and off road suspension and tires, interior heating, extending roof for mounting an outdoor terrace, 2 bike racks'),
+(31, 67862, 2, '145$', '4WD and off road suspension and tires, interior heating, extending roof for mounting an outdoor terrace, 2 bike racks'),
+(32, 41757, 2, '470$', '5 star luxury, extendable terrace roof, home cinema, residential regrigerator with ice dispencer, fireplace, buffet, extra storage');
+
+INSERT INTO `nordic-motorhome`.`motorhomes` VALUES
+(1, 'Fleetwood RV', 'Peace Arrow', 1),
+(2, 'Fleetwood RV', 'Peace Arrow', 2),
+(3, 'Fleetwood RV', 'Peace Arrow', 3),
+(4, 'Fleetwood RV', 'Peace Arrow', 4),
+(5, 'Fleetwood RV', 'Dicovery', 5),
+(6, 'Fleetwood RV', 'Dicovery', 6),
+(7, 'Fleetwood RV', 'Dicovery', 7),
+(8, 'Fleetwood RV', 'Southwind', 8),
+(9, 'Fleetwood RV', 'Southwind', 9),
+(10, 'Fleetwood RV', 'Southwind', 10),
+(11, 'Fleetwood RV', 'Flair', 11),
+(12, 'Fleetwood RV', 'Flair', 12),
+(13, 'Fleetwood RV', 'Flair', 13),
+(14, 'Fleetwood RV', 'Dicovery LXE', 14),
+(15, 'Fleetwood RV', 'Dicovery LXE', 15),
+(16, 'American Coach', 'Patriot', 16),
+(17, 'American Coach', 'Patriot', 17),
+(18, 'American Coach', 'Patriot', 18),
+(19, 'American Coach', 'Revolution', 19),
+(20, 'American Coach', 'Revolution', 20),
+(21, 'American Coach', 'Revolution', 21),
+(22, 'American Coach', 'Dream', 22),
+(23, 'American Coach', 'Dream', 23),
+(24, 'American Coach', 'Dream', 24),
+(25, 'Winnebago', 'Solis', 25),
+(26, 'Winnebago', 'Solis', 26),
+(27, 'Winnebago', 'Solis', 27),
+(28, 'Winnebago', 'Solis', 28),
+(29, 'Winnebago', 'Revel', 29),
+(30, 'Winnebago', 'Revel', 30),
+(31, 'Winnebago', 'Revel', 31),
+(32, 'Winnebago', 'Horizon', 32);
+
+INSERT INTO `nordic-motorhome`.`customer_contacts` VALUES
+( 1, 'dob@kea.dk','+45 12 34 56 78'),
+( 2, 'cahl@kea.dk','+45 87 65 43 21'),
+( 3, 'MrFancy@kea.dk','+45 11 12 13 14'),
+( 4, 'Antonio@kea.dk','+1 (809) 223 6156'),
+( 5, 'MrsFancy@kea.dk','+45 21 22 23 24'),
+( 6, 'alex154r@stud.kea.dk','+45 25 26 27 28'),
+( 7, 'rasm63f4@stud.kea.dk','+45 11 22 33 44'),
+( 8, 'lara0880@stud.kea.dk','+45 55 66 77 88'),
+( 9, 'cris1450@stud.kea.dk','+45 69 69 69 69');
+
+INSERT INTO `nordic-motorhome`.`customer_addresses` VALUES
+( 1, 'Lygten', 16, 'Copenhagen', 'Denmark', 2400),
+( 2, 'Lygten', 16, 'Copenhagen', 'Denmark', 2400),
+( 3, 'Fancy st.', 13, 'Wonderland', 'Atlantis', 10000),
+( 4, 'Royal ave.', 73, 'New York', 'U.S.A.', 13576),
+( 5, 'Fancy st.', 13, 'Wonderland', 'Atlantis', 10000),
+( 6, 'Lygten', 16, 'Copenhagen', 'Denmark', 2400),
+( 7, 'Lygten', 16, 'Copenhagen', 'Denmark', 2400),
+( 8, 'Lygten', 16, 'Copenhagen', 'Denmark', 2400),
+( 9, 'Lygten', 16, 'Copenhagen', 'Denmark', 2400);
+
+INSERT INTO `nordic-motorhome`.`customers` VALUES
+(1, 'Douglas','Beaver', 1, 1),
+(2, 'Cay','Larsen', 2, 2),
+(3, 'Mister','Fancy', 3, 3),
+(4, 'Antonio','Banderas', 4, 4),
+(5, 'Missis','Fancy', 5, 5),
+(6, 'Alexandru','Gabriel', 6, 6),
+(7, 'Rasmus','Berg', 7, 7),
+(8, 'Lara','Čulič', 8, 8),
+(9, 'Alin','Cristian', 9, 9);
+
+
+INSERT INTO `nordic-motorhome`.`pick-up_and_drop-off` VALUES
+( 1, '-', 0, '-', 0),
+( 2, '-', 0, '-', 0),
+( 3, 'CPH Lufthavn', 7, '-', 0);
+
+INSERT INTO `nordic-motorhome`.`terms_and_conditions` VALUES
+( 1, 'Low', 7, 42100, '65%', '-', 'No damage upon return.'),
+( 2, 'Low', 3, 4600, '52%', '-', 'No damage upon return.'),
+( 3, 'Middle', 14, 5000, '55%', '-', 'No damage upon return.');
+
+INSERT INTO `nordic-motorhome`.`contracts` VALUES
+( 1, 1, 1, 32, 32, 1, 1, 1),
+( 2, 2, 2, 30, 30, 2, 2, 2),
+( 3, 3, 3, 19, 19, 8, 8, 8);
+
+
+
+
