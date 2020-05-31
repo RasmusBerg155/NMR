d4d537d44299d57fdf11751cbfba3403cc64eafe
Rasmus <Rasmusmberg@gmail.com>
2020-05-31 15:28:08 +0200

    Sql code fix

--- src/main/java/com/demo/nmr/Repository/PickUpDropOffRepo.java
+++ src/main/java/com/demo/nmr/Repository/PickUpDropOffRepo.java
@@ -15,13 +15,13 @@ public class PickUpDropOffRepo {
     JdbcTemplate template;
 
     public List<PickUpDropOff> fetchAll(){
-        String sql = "SELECT * FROM `pick-up_and_drop-off`;";
+        String sql = "SELECT * FROM `pick_up_and_drop_off`;";
         RowMapper<PickUpDropOff> rowMapper = new BeanPropertyRowMapper<>(PickUpDropOff.class);
         return template.query(sql, rowMapper);
     }
 
     public PickUpDropOff addPickUpDropOff(PickUpDropOff p){
-        String sql = "INSERT INTO `pick-up_and_drop-off`(pd_id, pick_up_location, pick_up_distance," +
+        String sql = "INSERT INTO `pick_up_and_drop_off`(pd_id, pick_up_location, pick_up_distance," +
                 "drop_off_location, drop_off_distance) VALUES (?, ?, ?, ?, ?)";
         template.update(sql, p.getPd_id(), p.getPick_up_location(), p.getPick_up_distance(),
                 p.getDrop_off_location(), p.getDrop_off_distance());
@@ -29,14 +29,14 @@ public class PickUpDropOffRepo {
     }
 
     public PickUpDropOff findPickUpDropOffById(int pd_id){
-        String sql = "SELECT * FROM `pick-up_and_drop-off` WHERE pd_id = ?";
+        String sql = "SELECT * FROM `pick_up_and_drop_off` WHERE pd_id = ?";
         RowMapper<PickUpDropOff> rowMapper = new BeanPropertyRowMapper<>(PickUpDropOff.class);
         PickUpDropOff p = template.queryForObject(sql, rowMapper, pd_id);
         return p;
     }
 
     public Boolean deletePickUpDropOff(int pd_id){
-        String sql ="DELETE FROM `pick-up_and_drop-off` WHERE pd_id = ?";
+        String sql ="DELETE FROM `pick_up_and_drop_off` WHERE pd_id = ?";
         return template.update(sql, pd_id) < 0;
     }
 
